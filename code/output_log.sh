d27ff1718a5f9871fb47a09796cbfbd9639c323a
arapower <slothwood@gmail.com>
2022-02-05 16:37:08 +0900

    Change log file name

--- code/output_log.sh
+++ code/output_log.sh
@@ -9,6 +9,6 @@ teardown (){
 stl_output_log () {
 	(
 		log_date # logfile -> "$STL_ROOT_DIR/stl.log"
-		log_date "$STL_ROOT_DIR/stl.log" # specify path of log file
+		# log_date "$STL_ROOT_DIR/stl.log" # specify path of log file
 	)
 }
