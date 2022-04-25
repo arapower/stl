407fa4b8416bd569f8252235abf12e2fa6f74a59
arapower <slothwood@gmail.com>
2022-04-25 18:26:38 +0900

    Fix error when using Free BSD Sed

--- code/mod/insert_var_for_stl.sh
+++ code/mod/insert_var_for_stl.sh
@@ -9,7 +9,11 @@ teardown (){
 stl_insert_var_for_stl_tdd () {
 	(
 	test_code_input="$STL_TEST_DATA_DIR/mod/insert_var_for_stl_test_code_input.txt"
-	test_code_output="$STL_TEST_DATA_DIR/mod/insert_var_for_stl_test_code_output.txt"
+	: 'Create expected output' && {
+		test_code_output=$(mktemp)
+		cat "$STL_TEST_DATA_DIR/mod/insert_var_for_stl_test_code_output.txt" |
+		sed "s;<current_dir>;${STL_ROOT_DIR};g" > "${test_code_output}"
+	}
 
 	assert_equal_file "\"${target}\" \"$test_code_input\"" "$test_code_output"
 	)
